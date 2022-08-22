eedba401f0d82dbb26fefce5b727bce88951c71b
Ksh <ksrivas199@gmail.com>
2022-08-22 06:11:31 +0530

    initial

--- client/src/App.js
+++ client/src/App.js
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import getWeb3 from "./getWeb3";
-import SimpleStorage from "./contracts/SimpleStorage.json";
+import Lottery from "./contracts/Lottery.json";
 import "./App.css";
 
 const App = () => {
@@ -8,7 +8,6 @@ const App = () => {
     web3: null,
     contract: null,
   });
-  const [storageValue, setStorageValue] = useState(null);
 
   useEffect(() => {
     const init = async () => {
@@ -16,10 +15,10 @@ const App = () => {
         const web3 = await getWeb3();
         const networkId = await web3.eth.net.getId();
 
-        const deployedNetwork = SimpleStorage.networks[networkId];
+        const deployedNetwork = Lottery.networks[networkId];
         console.log("Contract Address:", deployedNetwork.address);
         const instance = new web3.eth.Contract(
-          SimpleStorage.abi,
+          Lottery.abi,
           deployedNetwork && deployedNetwork.address
         );
         setState({ web3, contract: instance });
@@ -31,18 +30,9 @@ const App = () => {
     init();
   }, []);
 
-  useEffect(() => {
-    async function getValue() {
-      const { contract } = state;
-      const value = await contract.methods.get().call();
-      setStorageValue(value);
-    }
-    state.contract && getValue();
-  }, [state.contract, state]);
-
   return (
     <div className="App">
-      <div>The stored values is: {storageValue}</div>
+      <div>Hello World</div>
     </div>
   );
 };
